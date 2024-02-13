f862e4071e0afd586780833df88da18e79d9b916
= <info@josevicentecarratala.com>
2024-02-13 18:13:09 +0100

    pseudocodigo

---
+++ examen.py
@@ -0,0 +1,17 @@
+
+
+# Primero mostramos el menu
+
+# Atrapamos la opción que ha seleccionado el usuario
+
+# Si ha elegido listar, muestrame el contenido del archivo de datos
+
+# Si ha elegido buscar, muestrame solo las entradas que coincidan
+
+# Si ha elegido insertar, pregunta los datos e inserta
+
+# Si ha elegido actualizar, pregunta cual se va a actualizar, pregunta datos, y reemplaza
+
+# Si ha elegido eliminar, pregunta criterio y elimina
+
+# Si ha elegido salir, sal
